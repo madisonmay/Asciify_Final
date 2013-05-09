f65ae862d1fcd58b46045c68bda821badd7af73e
jacobkingery <jacob.kingery@students.olin.edu>
2013-05-09 12:51:11 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from os import system
 
 dependencies = 'imagemagick ffmpeg gimp mencoder konsole '
 dependencies += 'python-opencv python-numpy libavcodec-extra-53 mpg123;'
-system('sudo apt-get install '+dependencies)
-system('sudo apt-get install youtube-dl;')
+system('sudo apt-get install ' + dependencies + ' youtube-dl -y;')
 system('chmod 755 fast_asciify')
 system('chmod 755 internal/trifecta')
 system('chmod 755 internal/asciifile')
