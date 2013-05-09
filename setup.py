6e891c994ad62edec86e73b1e584ddf0db1ca478
Bill Warner <billwarner13@gmail.com>
2013-05-08 22:01:37 -0400

    adding in asciifile

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ dependencies = 'imagemagick ffmpeg gimp mencoder konsole '
 dependencies += 'python-opencv python-numpy libavcodec-extra-53 mpg123;'
 system('sudo apt-get install '+dependencies)
 system('sudo apt-get install youtube-dl;')
-system('chmod 755 fast_asciify WebCam/trifecta')
+system('chmod 755 fast_asciify')
+system('chmod 755 internal/trifecta')
+system('chmod 755 internal/asciifile')
